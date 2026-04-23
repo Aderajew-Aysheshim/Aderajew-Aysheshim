c67a4dea068714b5db2dd16d198c89ba36352253
aderajew Aysheshim <aysheshimaderajew600@gmail.com>
2026-04-23 13:19:39 +0300

    Delete Desktop directory

--- Desktop/bookmanagment/books/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Book
-
-class BookForm(forms.ModelForm):
-    published_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
-
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'published_date', 'isbn']
